03ba75db540c96974d32101f8d55d7a308f53f39
emzio <emziolkow@gmail.com>
2022-04-07 15:20:56 +0200

    próby dodania baazy do security

--- src/main/java/pl/coderslab/config/SecurityConfig.java
+++ src/main/java/pl/coderslab/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package pl.coderslab.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import pl.coderslab.service.SpringDataUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -31,5 +34,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/user/start")
                 .and().logout().logoutSuccessUrl("/")
                 .and().exceptionHandling().accessDeniedPage("/403");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SpringDataUserDetailsService customUserDetailsService() {
+        return new SpringDataUserDetailsService();
     }
 }
---
+++ src/main/java/pl/coderslab/service/UserService.java
@@ -0,0 +1,11 @@
+package pl.coderslab.service;
+
+import org.springframework.stereotype.Service;
+import pl.coderslab.entity.User;
+
+
+public interface UserService {
+    User findByUserName(String name);
+
+    void saveUser(User user);
+}
