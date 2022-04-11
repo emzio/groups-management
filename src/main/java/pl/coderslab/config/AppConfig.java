441ff309f21fefc66a6454bc0a235737d0dfca1c
emzio <emziolkow@gmail.com>
2022-04-11 13:27:39 +0200

    rozpoczęcie łączeniea user z customer

--- src/main/java/pl/coderslab/config/AppConfig.java
+++ src/main/java/pl/coderslab/config/AppConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//import pl.coderslab.converter.CanceledClassesConverter;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
