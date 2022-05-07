72788d8780bc2e4c7f28e7a4154959ff3f0649bb
emzio <emziolkow@gmail.com>
2022-05-07 09:35:59 +0200

    zmiana sortowania CanceledClasses na BazęDanych i dodany poprawiony plik sample.sql

--- src/main/resources/sample.sql
+++ src/main/resources/sample.sql
@@ -4,8 +4,8 @@ INSERT INTO canceled_classes (local_date) VALUES ('2022-04-29');
 INSERT INTO canceled_classes (local_date) VALUES ('2022-02-15');
 
 
-INSERT INTO group_model (day_of_week, local_time, name, size) VALUES (1, '18:00:00', 'new name', 6);
-INSERT INTO group_model (day_of_week, local_time, name, size) VALUES (4, '18:00:00', 'nameTest', 6);
+INSERT INTO group_model (day_of_week, local_time, name, payment_rate, size) VALUES (3, '20:00:00', 'środa 19:30', 110.00, 6);
+INSERT INTO group_model (day_of_week, local_time, name, payment_rate, size) VALUES (2, '19:30:00', 'wtorek 19:30', 110.00, 6);
 
 INSERT INTO `role` (`id`, `name`) VALUES (NULL, 'ROLE_ADMIN');
 INSERT INTO `role` (`id`, `name`) VALUES (NULL, 'ROLE_USER');
